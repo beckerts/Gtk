d27a029b0600c2892982b4903dac7788ec44f2cd
Steve Beckert <stevebeckert@hotmail.com>
2019-01-19 18:31:49 -0800

    Read pin to set from command line

--- gtkmm/Pi2Console/test.cpp
+++ gtkmm/Pi2Console/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 #include "gpio.hpp"
 
@@ -9,11 +10,23 @@ int main(int argc, char* argv[])
     GPIO gpio;
     GPIO::IO io;
     int gpno=5;
+    string pparm = "-p";
+
+    cout << "argc = " << argc << endl;
+    for (int i =0; i<argc; i++)
+    {
+        cout << "argv[" << i<< "] = " << argv[i] << endl;
+    }
+    if (argc >= 3 && pparm.compare( argv[1]) == 0)
+    {
+        cout << "converting " << argv[2] << " to int." << endl;
+        istringstream convert(argv[2]);
+        if (!(convert >> gpno)) gpno = 0;
+    }
     cout << "gpio declared" << endl;
     cout << "io declared"<< endl;
     cout << "gpno declared: " << gpno << endl;
     cout << "value: " << gpio.read(gpno) << endl;
-    gpio.alt_function(gpno,io);
-    cout << GPIO::alt_name(io) << endl;
+    
 	return 0;
 }
